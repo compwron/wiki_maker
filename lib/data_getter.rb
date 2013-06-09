5e2216238020c52895355c5782c80439bf6201e3
compwron <wordymusic@gmail.com>
2013-06-08 19:29:40 -0500

    searching for passed-in name but not yet putting it into the generated wiki markup

--- lib/data_getter.rb
+++ lib/data_getter.rb
@@ -4,7 +4,12 @@ class DataGetter
 	# this is intended to be the layer that somehow googles a passed-n name and creates a Person object
 	def initialize(name)
 		# must put + between space-separated items
-		@sample_data = JSON.parse(`curl --silent "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=thoughtworks"`)
+		concatenated_name = concatenate_name(name)
+		@sample_data = JSON.parse(`curl --silent "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=#{concatenated_name}"`)
+	end
+
+	def concatenate_name(name)
+		name.split(" ").join("+")
 	end
 
 	def person
