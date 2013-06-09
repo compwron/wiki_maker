9c1f0a885ecc60df29108c9ef01ba9472335beb8
compwron <wordymusic@gmail.com>
2013-06-09 17:55:37 -0500

    comment indicating intent to recurse on search

--- lib/data_getter.rb
+++ lib/data_getter.rb
@@ -9,6 +9,14 @@ class DataGetter
 		api_url = "https://api.duckduckgo.com/?q=#{concatenated_name}&format=json"
 		# api_url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q="
 		@sample_data = JSON.parse(`curl --silent "#{api_url}"`)
+
+		find_more_data()
+	end
+
+	def find_more_data
+		# if born on and nationality are empty, keep googling
+		# need a time-out so it doesn't go on forever if there's nothing to find
+		
 	end
 
 	def concatenate_name(name)
